4586e7bef04fb62d8272b3f0b046d50e2e0899dd
Khwerz <khwerz@gmail.com>
2020-02-23 17:32:42 -0400

    +F Commands/Simple/cat.lua

---
+++ Commands/Simple/cat.lua
@@ -0,0 +1,22 @@
+#!/home/lan/Run/CUI/lua
+
+-- bufsize 8192
+function cat(file,bufsize)
+ if not bufsize then bufsize=8192 end
+ local f = io.open(file)
+ if not f then return false end
+ local buffer
+ for buffer in f:lines(bufsize) do
+  io.write( buffer )
+ end
+ io.close(f)
+ return true
+end
+
+if #arg==0 then
+ arg[1]="/dev/stdin"
+end
+
+for i,File in ipairs(arg) do
+ cat(File)
+end
