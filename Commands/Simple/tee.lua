bf576d235af4c6a82b68d0ecfd4d9e3fd32e48b9
Khwerz <khwerz@gmail.com>
2020-02-23 22:41:15 -0400

    +F Commands/Simple/tee.lua

---
+++ Commands/Simple/tee.lua
@@ -0,0 +1,34 @@
+#!/home/lan/Run/CUI/lua
+
+-- tee.lua [-mode files]
+-- saves input to files
+-- writes to standard output
+
+do
+ local iarg = 1
+ local modematch = "[wa]%+?b?"
+ local mode = "w"
+ if #arg>0 and string.find(arg[1],"^-".. modematch .. "$") then
+  iarg = 2
+  mode = string.match(arg[1],"^%-(" .. modematch .. ")$")
+ end
+
+ files = {}
+ local f
+ for i=iarg,#arg do
+  f = io.open(arg[i],mode)
+  if f then
+  -- f:setvbuf("full") -- maybe?
+  -- print(arg[i]) opened file
+   files[1+#files] = f
+  end
+ end
+end
+
+for line in io.input():lines("L") do
+ io.write(line)
+ for i,f in ipairs(files) do
+  f:write(line)
+  -- f:flush()
+ end
+end
