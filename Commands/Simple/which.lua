21b62d523a4f112887236dc670352bd5ef708f9f
Khwerz <khwerz@gmail.com>
2020-03-02 14:50:34 -0400

    which supports -a now and exit status

--- Commands/Simple/which.lua
+++ Commands/Simple/which.lua
@@ -1,44 +1,65 @@
 #!/usr/bin/env lua
 
--- basically tests if args exist in $path
--- doesn't care about permissions
+-- Synopsis basically tests if args exist in $path
+-- BUG doesn't care about permissions
 
--- returns true/false if file is readable
+-- -a returns all files
+-- it exits 0 if it finds all args in path
+-- exits 1 if it found some
+-- exits 2 for no args
+
+
+-- returns true/false if file is exists
 function test(file)
  local f = io.open(file)
  if f then
   io.close(f)
+  print(file)
   return true
  end
  return false
 end
 
--- returns $file/path or nil if not found
-function finpathall(file)
- local x
- for i,p in ipairs(path) do
-  x = p .. "/" .. file
-  if test(x) then print(x) end
- end
-end
+-- returns true if it finds file in path
+function findInPath(file,stop)
+ local r = false
 
-function finpath(file)
- local x
- for i,p in ipairs(path) do
-  x = p .. "/" .. file
-  if test(x) then print(x) break end
- end
-end
+ if stop then -- just one file
 
+  for i,p in ipairs(path) do
+   p = p .. "/" .. file
+   if test(p) then r=true break end
+  end
 
-if #arg then
- path = {}
- for p in string.gmatch(os.getenv("PATH"),"[^:]+") do
-  path[1+#path] = p
- end
- if arg[1]=="-a" then
-  for i=2,#arg do finpathall(arg[i]) end
  else
-  for i=1,#arg do finpath(arg[i]) end
+
+  for i,p in ipairs(path) do
+   p = p .. "/" .. file
+   if test(p) then r=true end
+  end
+
+ end
+ return r
+end
+
+e=2
+if #arg>0 and not (#arg==1 and arg[1]=="-a") then
+
+ path = {} -- $PATH to path{}
+ for p in os.getenv("PATH"):gmatch("[^:]+") do
+  path[#path+1] = p
+ end
+
+ local s,x = 1,true
+ if arg[1]=="-a" then
+  s=2
+  x=false
+ end
+ 
+ e=0
+ for i=s,#arg do
+  if not findInPath(arg[i],x) then e=1 end
  end
 end
+os.exit(e)
+
