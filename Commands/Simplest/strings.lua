870697f42f0dbb70e11215f83d9281900c58185f
Khwerz <khwerz@gmail.com>
2020-03-23 14:10:15 -0400

    strings.lua direct string matching using repetitions instead of string length comparisons

--- Commands/Simplest/strings.lua
+++ Commands/Simplest/strings.lua
@@ -1,7 +1,11 @@
 #!/usr/bin/env lua
 
--- todo, UTF8 support
-strsize = 3
+--[[ TODO
+ error if strsize==0 or 1
+ UTF8?
+]]
+
+strsize = 4
 bufsize = 8192
 
 -- debugging functions I'm afraid of taking out
@@ -13,36 +17,53 @@ function inspect( s )
  end
 end
 
+-- returns pattern/str with repetitions
+function repat(charset,rep) -- lua has no {4} support
+ local pat = charset
+ for i=2,rep do -- i=2 because pat is defined
+  pat = pat .. charset
+ end
+ return pat
+end
+
 function all( f )
- local str = f:read("*a")
- for s in str:gmatch("[%g \t]+") do -- newlines mess everything
-  if string.len(s)>strsize then print(s) end
+ for s in string.gmatch(
+  f:read("*a") or "",
+  repat("[ -~\t]",strsize) .. "+"
+ ) do
+  print(s)
  end
 end
 
-
 function buffered( f )
+-- Patterns
+ local charset = "[ -~\t]"
+ local notcharset = "[^ -~\t]"
+
+ -- basically (charset{strsize,})[^charset]
+ local pattern = "(" .. repat(charset,strsize) .. "+)" .. notcharset
+
  local roll -- rollover from last buffer read
  for str in f:lines(bufsize) do
 
-  if roll then -- might get full
-   if string.find(roll .. str,"[^ -~]") then
-    str = roll .. str -- leave the mess to the main loop
-   else  -- its a huge string, write everything out
-    if string.len(roll)>strsize then
-     io.write(roll) -- I am unsure if this makes sense
-    end
+  if roll then
+   if string.len(roll) > (bufsize+strsize) then -- roll wont get too big hopefully
+    io.write( string.sub(roll,1,bufsize) )
+    str = string.sub(roll,bufsize+1) .. str
+   else
+    str = roll .. str
    end
    roll = nil
   end
-  
+
+
 -- captured is printable characters, will skip [.\n]$
-  for s in str:gmatch("([ -~\t]+)[^ -~]") do
-   if string.len(s)>strsize then print(s) end
+  for s in string.gmatch(str,pattern) do
+   print(s)
   end
   
 -- capture skipped part, to be used in the next buffer read as a continuation
-  roll = str:match("[ -~\t]+$") -- a string without newlines will fill roll
+  roll = str:match(charset .. "+$")
  end
  
  if roll and
@@ -58,9 +79,10 @@ function strings_file( f )
  local fsize = f:seek("end")
   f:seek("set")
  if fsize<bufsize then
---  io.stderr:write("all\n")
+  io.stderr:write("all\n")
   all( f )
- else -- io.stderr:write("buffered\n")
+ else
+  io.stderr:write("buffered\n")
   buffered( f )
  end
  io.close(f)
@@ -70,9 +92,9 @@ if #arg==0 then
  buffered(io.stdin) -- can't know size, use strings_buffered
 else
  local f
-
- for i,v in ipairs(arg) do
-  f = io.open(v) -- print error for new file?
+-- TODO: getflags here
+ for i,file in ipairs(arg) do
+  f = io.open(file, "rb") -- print error for new file?
 
   if f then
    strings_file(f)
@@ -81,3 +103,7 @@ else
  end
 
 end
+
+-- its really slow compared to gnu strings, and plan9 strings is slower than gnu
+-- but its negligible, when you use it with small binaries (<10mb?)
+-- os.execute("ps -C lua u")
